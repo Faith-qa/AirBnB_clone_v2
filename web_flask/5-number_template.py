2ea247f5ec4faafffbdf771587ea26b8b1902dd8
Faith-qa <atienofaith12@gmail.com>
2021-07-04 02:16:55 +0300

    corrected pep8 errors

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,6 @@ def n_is_a_number(n):
     return "{:d} is a number".format(n)
 
 
-
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', num=n)
